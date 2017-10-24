bedc68bf3c1921de3f899b378965ec62af9eb5a3
User <user@li1stfloorimac1-1.lehman.edu>
2017-10-23 23:09:37 -0400

    Initial Commit

---
+++ slot machine mid/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  slot machine mid
+//
+//  Created by User on 10/23/17.
+//  Copyright © 2017 User. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ slot machine midTests/slot_machine_midTests.swift
@@ -0,0 +1,36 @@
+//
+//  slot_machine_midTests.swift
+//  slot machine midTests
+//
+//  Created by User on 10/23/17.
+//  Copyright © 2017 User. All rights reserved.
+//
+
+import XCTest
+@testable import slot_machine_mid
+
+class slot_machine_midTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
